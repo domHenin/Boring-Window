f0170aae0326bb0c6a020e718d4751b56dfe8587
domHenin <domhenin.517@gmail.com>
2019-02-20 11:57:41 -0500

    created window

---
+++ src/io/cloudlogix/Project/Main.java
@@ -0,0 +1,35 @@
+package io.cloudlogix.Project;
+
+import java.awt.color.*;
+import java.util.HashMap;
+import java.util.Map;
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {g
+//        System.out.println("Hello, World");
+        JFrame jFrame = new JFrame();
+
+        jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        jFrame.setTitle("JAVA ROCKS");
+        jFrame.setSize(300, 300);
+        jFrame.setVisible(true);
+
+    }
+}
+
+//set JFrame.
+//setDef.
+//setSiz.
+//setVis.
+/*
+ *class windowMain extends JFrame{
+	public static void main(String[] args) {
+		JFrame f = new windowMain();
+		f.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		f.setSize(300, 200);
+		f.setTitle("JAVA ROCKS!!");
+		f.setBackground(Color.BLACK);
+		f.setVisible(true);
+ */
+
